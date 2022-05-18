408f6b7ed9792cf20734e787161bc89b03265db7
Filip Crnobrnja <fica.c@yandex.ru>
2022-05-19 02:02:55 +0300

    [AVM] Start project tools integration
    * Add compile system for release, debug, profile, final
    * Integrate easyloggingpp
    * Generate Makefile
    * Add precomp files
    * Add main

---
+++ premake5.lua
@@ -0,0 +1,79 @@
+workspace "abstract-vm"
+    configurations { "Release", "Debug", "Profile", "Final" }
+
+outputdir = "%{cfg.buildcfg}"
+
+project "avm"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++latest"
+    toolset "gcc"
+    targetdir ("bin/" .. outputdir)
+    objdir ("tmp/" .. outputdir)
+    
+    pchheader "source/precomp.h"
+    pchsource "source/precomp.cpp"
+
+    includedirs
+    {
+        "extern/logging/easyloggingpp/src",
+        "source"
+    }
+
+    defines
+    {
+        "AUTO_INITIALIZE_EASYLOGGINGPP",
+        "AVM_EASYLOGGINGPP_DEFINED"
+    }
+
+    files
+    {
+        "extern/logging/easyloggingpp/src/easylogging++.h",
+        "extern/logging/easyloggingpp/src/easylogging++.cc",
+        "source/**.h",
+        "source/**.cpp"
+    }
+
+    filter "configurations:Release"
+        defines
+        {
+            "DEBUG",
+            "AVM_RELEASE"
+        }
+        warnings "Extra"
+        symbols "On"
+        optimize "On"
+
+    filter "configurations:Debug"
+        defines
+        {
+            "DEBUG",
+            "AVM_DEBUG"
+        }
+        symbols "On"
+        optimize "Off"
+
+    filter "configurations:Profile"
+        defines
+        {
+            "NDEBUG",
+            "AVM_PFOFILE"
+        }
+        symbols "Off"
+        optimize "On"
+
+    filter "configurations:Final"
+        defines
+        {
+            "NDEBUG",
+            "AVM_FINAL"
+        }
+        flags
+        {
+            "FatalCompileWarnings",
+            "FatalLinkWarnings",
+            "FatalWarnings"
+        }
+        warnings "Extra"
+        symbols "Off"
+        optimize "On"
---
+++ source/main/main.cpp
@@ -0,0 +1,8 @@
+#include "precomp.h"
+
+int main(int argc, char **argv)
+{
+    START_EASYLOGGINGPP(argc, argv);
+
+    return (0);
+}
